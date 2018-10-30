e50589fdba89569f1c23fb708575cc3331f6ee57
BuildTools <ensup_choi@aol.com>
2018-10-30 18:53:04 +0900

    대문자 지원

--- Calculator.py
+++ Calculator.py
@@ -1,8 +1,14 @@
 e="n"
+if e=='N':
+    e='n'
 while e=='n':
     print('*This Calculator is only for addition*')
     f=int(input("Type first number:"))
     l=int(input("Type second number:"))
     a=f+l
     print(a)
-    e=input("Do you want to exit?[Y,N]:")
+    e=input("Do You want to exit?[Y,N]")
+    if e=='N':
+        e='n'
+    
+
